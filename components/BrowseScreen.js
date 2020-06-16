1500c45cce59fc79685e2f35723d7f8b2f3adcde
Afshin Teymoori <afshin_tm@yahoo.com>
2020-06-16 21:37:51 +1000

    flatlist and image

--- components/BrowseScreen.js
+++ components/BrowseScreen.js
@@ -16,14 +16,26 @@ import Constants from 'expo-constants';
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    title: 'Item1',
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97ft56',
     title: 'Second Item',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: 'bdhyer-c1b1-46c2-aed5-3ad5b28h44k98ba',
+    title: 'First Item',
+  },
+  {
+    id: '38877984-c605-48d3-a4f8-fb365aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '9636474-d221-471f-bd96-31e29d72765',
     title: 'Third Item',
   },
 ];
@@ -31,6 +43,9 @@ const DATA = [
 function Item({ title }) {
   return (
     <View style={styles.item}>
+      <Image  style={styles.tinyLogo}
+        source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}>
+      </Image>
       <Text style={styles.title}>{title}</Text>
     </View>
   );
