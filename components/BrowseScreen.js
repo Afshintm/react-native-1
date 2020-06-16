b7a34d85573e3207aac592c086692d72b2923550
Afshin Teymoori <afshin_tm@yahoo.com>
2020-06-16 18:56:39 +1000

    using scroll view

--- components/BrowseScreen.js
+++ components/BrowseScreen.js
@@ -2,26 +2,56 @@ import React from "react";
 import { 
     View,
     Text,
-    StyleSheet,Button
+    StyleSheet,
+    Button, 
+    ScrollView,
+    Image
 } from "react-native";
 function BrowseScreen({navigation}) {
+  const images = [
+    {src:'../assets/splash.png'},
+    {src:'../assets/splash.png'},
+    {src:'../assets/splash.png'},
+    ];
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Browse Screen</Text>
         <Button
         title="Take Photo"
-        onPress={() => navigation.navigate('TakePhoto')}
-      />
+        onPress={() => navigation.navigate('TakePhoto')}/>
+        <ScrollView style={styles.container}>
+          {
+            images.forEach((i)=>{
+              console.log(i.src);
+            })
+
+          }
+          
+        </ScrollView>
+        <Image src="../assets/splash.png"></Image>
       </View>
     );
   }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+  },
+  boxSmall: {
+    width: 200,
+    height: 200,
+    marginBottom: 10,
+    marginRight: 10,
+    backgroundColor: 'skyblue',
+  },
+  boxLarge: {
+    width: 300,
+    height: 300,
+    marginBottom: 10,
+    marginRight: 10,
+    backgroundColor: 'steelblue',
+  },
 });
+
 export default BrowseScreen;
---
+++ components/DisplayAnImage.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 100,
+    height: 100,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
+
+const DisplayAnImage = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={require('@expo/snack-static/react-native-logo.png')}
+      />
+    </View>
+  );
+}
+
+export default DisplayAnImage;
