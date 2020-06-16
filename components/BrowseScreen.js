3a3da27fd7ba8c2a039fb0f9be71159ab92ebb70
Afshin Teymoori <afshin_tm@yahoo.com>
2020-06-16 20:17:40 +1000

    add flat list

--- components/BrowseScreen.js
+++ components/BrowseScreen.js
@@ -9,6 +9,32 @@ import {
     SafeAreaView, 
     FlatList
 } from "react-native";
+import Constants from 'expo-constants';
+
+
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
 function BrowseScreen({navigation}) {
   const images = [
     {src:'../assets/splash.png'},
@@ -16,22 +42,15 @@ function BrowseScreen({navigation}) {
     {src:'../assets/splash.png'},
     ];
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Browse Screen</Text>
-        <Button
-        title="Take Photo"
-        onPress={() => navigation.navigate('TakePhoto')}/>
-        {/* <ScrollView style={styles.container}>
-          {
-            images.forEach((i)=>{
-              console.log(i.src);
-            })
-
-          }
-          
-        </ScrollView> */}
-        <Image style={styles.tinyLogo} source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}></Image>
-      </View>
+      <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+        numColumns={2}
+        
+      />
+      </SafeAreaView>
     );
   }
 
@@ -39,6 +58,7 @@ function BrowseScreen({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: Constants.statusBarHeight,
   },
   boxSmall: {
     width: 200,
@@ -57,6 +77,15 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 100,
     height: 100,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 5,
+    marginVertical: 5,
+    marginHorizontal: 5,
+  },
+  title: {
+    fontSize: 32,
   },
 });
 
